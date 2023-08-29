a23e81f53cb4c7d18bf3520e2609035d856f1105
Aimene AHDIBI <aimene.ahdibi@orange.com>
2023-08-29 15:33:37 +0200

    [Feat] add selenium screener

--- default_config.py
+++ default_config.py
@@ -1,20 +1,53 @@
-from dotenv import load_dotenv
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
 from logging.config import dictConfig
 
-load_dotenv(dotenv_path=".env")
+from dotenv import load_dotenv
 
 
 class DefaultConfig:
+    load_dotenv(dotenv_path=".env")
+
+    DEBUG = os.environ["DEBUG"]
+    BUNDLE_ERRORS = True
+
+    PREFIX_PATH = "/{}".format(os.environ["DEPLOYMENT_VERSION"])
+
+    SCRAPPER_API_KEY = os.environ["SCRAPPER_API_KEY"]
+
+    PROPAGATE_EXCEPTIONS = True
+
+    INSTANCE_PATH = os.environ["INSTANCE_PATH"]
+    SNAPSHOTS_PATH = os.environ["SNAPSHOTS_PATH"]
+
+    REQUESTS_HEADERS = {
+        "User-agent": (
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36"
+            " (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36"
+        ),
+    }
+
+    SNAPSHOT_HTML_TIMEOUT = int(os.environ["SNAPSHOT_HTML_TIMEOUT"])
+
+    # Selenium config
     BROWSER_EXIT_TIMEOUT = 60
-    PROXY_HOST = None
-    PROXY_USERNAME = None
-    PROXY_PASSWORD = None
+    SNAPSHOT_BROWSER_WINDOW_HEIGHT = 800
+    SNAPSHOT_BROWSER_WINDOW_WIDTH = 1200
+    DEFAULT_SELENIUM_BROWSER = os.environ["DEFAULT_SELENIUM_BROWSER"]
     ENABLE_SELENIUM_PROXY = False
-    SNAPSHOT_BROWSER_WINDOW_WIDTH = 800
-    SNAPSHOT_BROWSER_WINDOW_HEIGHT = 1200
     INITIAL_PAGE_LOAD_TIME_MIN = 5
     INITIAL_PAGE_LOAD_TIME_MAX = 15
-    DEFAULT_SELENIUM_BROWSER = "firefox"
+    # ROUND-ROBIN PROXY
+    PROXY_HOST = os.environ["PROXY_HOST"]
+    PROXY_USERNAME = os.environ["PROXY_USERNAME"]
+    PROXY_PASSWORD = os.environ["PROXY_PASSWORD"]
+
+    # web drivers executable PATHS
+    # automatically assigned before the first request
+    FIREFOX_EXECUTABLE_PATH = os.environ["FIREFOX_EXECUTABLE_PATH"]
+    CHROME_EXECUTABLE_PATH = os.environ["CHROME_EXECUTABLE_PATH"]
 
     @staticmethod
     def init_loggers():
@@ -34,13 +67,13 @@ class DefaultConfig:
                 "console": {
                     "level": "DEBUG",
                     "class": "logging.StreamHandler",
-                    "stream": "ext://sys.stdout",
+                    "stream": "ext://flask.logging.wsgi_errors_stream",
                     "formatter": "verbose",
                 },
                 "file": {
                     "level": "INFO",
                     "class": "logging.handlers.RotatingFileHandler",
-                    "filename": "/tmp/dockerized_scrapper.log",
+                    "filename": "/tmp/web_scrapper.log",
                     "mode": "a",
                     "maxBytes": 10485760,
                     "backupCount": 10,
@@ -48,7 +81,11 @@ class DefaultConfig:
                 },
             },
             "loggers": {
-                "scrapper": {
+                "web_scrapper": {
+                    "level": "INFO",
+                    "handlers": ["console", "file"],
+                },
+                "flask_restx": {
                     "level": "INFO",
                     "handlers": ["console", "file"],
                 },
--- selenium_screener.py
+++ selenium_screener.py
@@ -2,58 +2,71 @@ import random
 import time
 from logging import getLogger
 
+from flask import current_app
 from selenium.common.exceptions import (
     NoAlertPresentException,
     TimeoutException,
     WebDriverException,
 )
-from default_config import DefaultConfig
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from seleniumwire import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
-from webdriver_manager.firefox import GeckoDriverManager
 
-logger = getLogger("pi_celery")
+logger = getLogger("web_scrapper")
 
 
 class Selenium:
     def __init__(
-            self,
-            browser="FIREFOX",
-            private=True,
-            headless=True,
-            insecure_cert=True,
-            enable_proxy=False,
+        self,
+        browser="FIREFOX",
+        private=True,
+        headless=True,
+        insecure_cert=True,
+        enable_proxy=False,
     ):
-        self.__exit_timeout = DefaultConfig.BROWSER_EXIT_TIMEOUT
-        self.__PROXY = DefaultConfig.PROXY_HOST
-        self.__username = DefaultConfig.PROXY_USERNAME
-        self.__password = DefaultConfig.PROXY_PASSWORD
+        self.__exit_timeout = current_app.config["BROWSER_EXIT_TIMEOUT"]
+        self.__OCD_PROXY = current_app.config.get("PROXY_HOST", None)
+        self.__username = current_app.config.get("PROXY_USERNAME", None)
+        self.__password = current_app.config.get("PROXY_PASSWORD", None)
 
-        if browser == "FIREFOX":
+        if browser.upper() == "FIREFOX":
             self.driver = self.generate_firefox_driver(
                 private, headless, insecure_cert, enable_proxy
             )
-        elif browser == "CHROME":
+        elif browser.upper() == "CHROME":
             self.driver = self.generate_chrome_driver(
                 private, headless, insecure_cert, enable_proxy
             )
         self.driver.delete_all_cookies()
         self.driver.set_window_size(
-            width=DefaultConfig.SNAPSHOT_BROWSER_WINDOW_WIDTH,
-            height=DefaultConfig.SNAPSHOT_BROWSER_WINDOW_HEIGHT,
+            width=current_app.config["SNAPSHOT_BROWSER_WINDOW_WIDTH"],
+            height=current_app.config["SNAPSHOT_BROWSER_WINDOW_HEIGHT"],
         )
 
-    def __get_proxy(self):
-        if not any([self.__username, self.__password, self.__PROXY]):
+    def __get_ocd_proxy(self):
+        if not any([self.__username, self.__password, self.__OCD_PROXY]):
             raise ValueError(
                 "The scrapper require proxy config to use it in proxy mode"
             )
-        http = "http://" + self.__username + ":" + self.__password + "@" + self.__PROXY
-        https = "https://" + self.__username + ":" + self.__password + "@" + self.__PROXY
+        http = (
+            "http://"
+            + self.__username
+            + ":"
+            + self.__password
+            + "@"
+            + self.__OCD_PROXY
+        )
+        https = (
+            "https://"
+            + self.__username
+            + ":"
+            + self.__password
+            + "@"
+            + self.__OCD_PROXY
+        )
         no_proxy = "localhost,127.0.0.1,dev_server:8080"
         proxy_options = {
             "proxy": {
@@ -65,16 +78,13 @@ class Selenium:
         return proxy_options
 
     def generate_chrome_driver(
-            self,
-            private=False,
-            headless=False,
-            insecure_certs=False,
-            enable_proxy=False,
-            block_popups=False,
+        self,
+        private=False,
+        headless=False,
+        insecure_certs=False,
+        enable_proxy=False,
     ):
         chromium_options = webdriver.ChromeOptions()
-        desired_caps = {}
-
         if headless:
             chromium_options.add_argument("-headless")
 
@@ -84,38 +94,29 @@ class Selenium:
         if private:
             chromium_options.add_argument("−−incognito")
 
-        if block_popups:
-            desired_caps["goog:chromeOptions"] = {
-                "excludeSwitches": ["disable-popup-blocking"]
-            }
-
-        executable_path = ChromeDriverManager(
-            chrome_type=ChromeType.CHROMIUM
-        ).install()
+        executable_path = current_app.config["CHROME_EXECUTABLE_PATH"]
         if enable_proxy:
-            proxy_options = self.__get_proxy()
+            proxy_options = self.__get_ocd_proxy()
             driver = webdriver.Chrome(
-                executable_path=executable_path,
+                service=ChromeService(executable_path),
                 seleniumwire_options=proxy_options,
-                desired_capabilities=desired_caps,
                 options=chromium_options,
             )
 
         else:
             driver = webdriver.Chrome(
-                executable_path=executable_path,
-                desired_capabilities=desired_caps,
+                service=ChromeService(executable_path),
                 options=chromium_options,
             )
 
         return driver
 
     def generate_firefox_driver(
-            self,
-            private=False,
-            headless=False,
-            insecure_certs=False,
-            enable_proxy=False,
+        self,
+        private=False,
+        headless=False,
+        insecure_certs=False,
+        enable_proxy=False,
     ):
         firefox_options = webdriver.FirefoxOptions()
         if headless:
@@ -127,19 +128,19 @@ class Selenium:
         if private:
             firefox_options.add_argument("-private")
 
-        executable_path = GeckoDriverManager().install()
+        executable_path = current_app.config["FIREFOX_EXECUTABLE_PATH"]
 
         if enable_proxy:
-            proxy_options = self.__get_proxy()
+            proxy_options = self.__get_ocd_proxy()
             driver = webdriver.Firefox(
-                executable_path=executable_path,
+                service=FirefoxService(executable_path),
                 seleniumwire_options=proxy_options,
                 options=firefox_options,
             )
 
         else:
             driver = webdriver.Firefox(
-                executable_path=executable_path,
+                service=FirefoxService(executable_path),
                 options=firefox_options,
             )
 
@@ -160,11 +161,11 @@ class Selenium:
             return return_code is None
 
     def take_snapshot(
-            self,
-            url="https://www.google.com",
-            add_initial_page_load_time=False,
-            html_path=None,
-            png_path=None,
+        self,
+        url="https://www.google.com",
+        add_initial_page_load_time=False,
+        html_path=None,
+        png_path=None,
     ):
         if not all((html_path, png_path)):
             raise ValueError(
@@ -175,8 +176,8 @@ class Selenium:
 
         if add_initial_page_load_time:
             loading_time = random.randint(
-                DefaultConfig.INITIAL_PAGE_LOAD_TIME_MIN,
-                DefaultConfig.INITIAL_PAGE_LOAD_TIME_MAX,
+                current_app.config["INITIAL_PAGE_LOAD_TIME_MIN"],
+                current_app.config["INITIAL_PAGE_LOAD_TIME_MAX"],
             )
             logger.info(
                 f"initial wait of {loading_time} seconds for page loading ...."
@@ -200,7 +201,7 @@ class Selenium:
         try:
             # HTML content snapshot
             html = self.driver.page_source
-            with open(html_path, "w") as fd:
+            with open(html_path, "w+") as fd:
                 fd.write(html)
                 fd.close()
 
@@ -229,5 +230,3 @@ class Selenium:
                 + " seconds ... exiting"
             )
             self.driver.quit()
-
-
